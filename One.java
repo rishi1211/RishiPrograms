6a4e8afcc406c18ef3540f77b0d72be53fd4f9a0
rishi1211 <lvrishi8@gmail.com>
2022-10-23 20:35:51 +0530

    first commoit of m1 method

---
+++ One.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class One {
+
+	public void m1() {
+		System.out.println("hello world");
+	}
+}
---
+++ Test.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ One o1=new One();
+ 
+ o1.m1();
+	}
+
+}
